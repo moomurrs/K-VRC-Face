d2adb129787669e908cf5d6124f647c0ddc77d9a
moomurrs <59849596+moomurrs@users.noreply.github.com>
2024-03-10 13:48:29 -0400

    refactor function names, add comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,17 @@
 #include <Wire.h>
 
 // forward declaration
-void draw_face(int x, int y, int width, int height, int radius);
-void blink();
+void draw_idle_face(int x, int y, int width, int height, int radius);
+void animate_face();
 void draw_mouth();
 #define SCREEN_WIDTH 128  // OLED display width,  in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
 // OLED display with address 0x3C for 128x64
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+int oled_addr = 0x3C;
 
+// face properties
 int eye_width = 20;
 int eye_height = 30;
 int eye_radius = 20;
@@ -25,7 +27,7 @@ void setup() {
     Serial.begin(115200);
     randomSeed(analogRead(0));
     // initialize display
-    if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    if (!oled.begin(SSD1306_SWITCHCAPVCC, oled_addr)) {
         Serial.println("SSD1306 allocation failed");
         while (true)
             ;
@@ -36,19 +38,18 @@ void setup() {
 }
 
 void loop() {
-    
-    draw_face(15, 10, eye_width, eye_height, eye_radius);
-    //oled.clearDisplay();
-    //oled.fillRoundRect(42, 50, 40, 10, 5, WHITE);
-    //oled.display();
-    int time = random(500, 3000);
-    Serial.println((String) "time: " + time);
+    // draw eyes fully open
+    draw_idle_face(15, 10, eye_width, eye_height, eye_radius);
+    // random blink interval
+    int time = random(500, 3000); 
+    // wait before animating eyes
     delay(time);
-
-    blink();
+    // draw eyes blinking
+    animate_face();
 }
 
-void draw_face(int x, int y, int width, int height, int radius) {
+// clear screen, draw eyes (fully open) + mouth
+void draw_idle_face(int x, int y, int width, int height, int radius) {
     oled.clearDisplay();
     // left eye
     oled.fillRoundRect(x, y, width, height, radius, WHITE);
@@ -62,14 +63,17 @@ void draw_face(int x, int y, int width, int height, int radius) {
     oled.display();
 }
 
+// statically draw mouth
 void draw_mouth(){
     int mouth_radius = 25;
     oled.fillRoundRect((SCREEN_WIDTH / 2) - mouth_radius, 53, mouth_radius * 2, 10, 5, WHITE);
     //oled.fillRoundRect(42, 50, 40, 10, 5, WHITE);
 }
 
-void blink() {
+// animates eyes closing and opening
+void animate_face() {
     int animation_speed = 13;
+    // close eye
     for (int i = eye_height; i >= 5; i -= animation_speed) {
         oled.clearDisplay();
         oled.fillRoundRect(left_eye_x, left_eye_y, eye_width, i, eye_radius,
@@ -82,6 +86,7 @@ void blink() {
 
     delay(10);
 
+    // open eye
     for (int i = 5; i <= eye_height; i += animation_speed) {
         oled.clearDisplay();
         oled.fillRoundRect(left_eye_x, left_eye_y, eye_width, i, eye_radius,
